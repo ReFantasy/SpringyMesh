e2a6e5e083f9253637e31ca347e2bab30d418ba2
ReFantasy <309741309@qq.com>
2021-04-13 21:41:15 +0800

    .

---
+++ main.py
@@ -0,0 +1,18 @@
+import taichi as ti
+import numpy as np
+import time
+
+ti.init(arch=ti.gpu)
+ti.init(device_memory_GB=0.1)
+
+gui = ti.GUI("TaichiGUI", res=(600, 600), background_color=0x00456B)
+
+# 添加滑块
+slider_value = gui.slider("slide", 0, 300, 1)
+slider_value.value = 50
+
+while True:
+    if gui.get_event(ti.GUI.ESCAPE):
+        break
+    gui.circle(pos=(0.5, 0.5), color=0xFF0000, radius=slider_value.value)
+    gui.show()
